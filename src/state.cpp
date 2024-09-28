cb97fe1ce390e9fca4109a0b2abbafc1e2510643
K Hawkins <kjh67@cantab.ac.uk>
2024-09-28 13:51:27 +0100

    Implemented constructors, getters, and setters for State class.

--- src/state.cpp
+++ src/state.cpp
@@ -1,8 +1,11 @@
+#include <algorithm>
 #include "aes.h"
+
+using namespace std;
 
 namespace aes {
 
-// CONSTRUCTORS
+// Constructors
 
 State::State() {
     nb = 4;
@@ -17,6 +20,42 @@ State::State(int block_size) {
     else {
         throw InitialisationError();
     }
+}
+
+
+// Getters and Setters
+
+void State::SetState(byte* content) {
+    // Check size compatibility
+    if (sizeof(state) == sizeof(content)) {
+        // Deep copy content to the state array
+        std::copy(content, content+nb*4, state);
+    }
+}
+
+byte* State::GetState() {
+    byte to_return[nb*4];
+    std::copy(state, state+nb*4, to_return);
+    return state;
+}
+
+
+// State manipulations
+
+void SubBytes() {
+    
+}
+
+void ShiftRows() {
+
+}
+
+void MixColumns() {
+
+}
+
+void AddRoundKey(byte* round_key) {
+
 }
 
 }
